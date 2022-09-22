b9fdb7a62642873520e9563e0bd72f0da8304548
KyleDerEchte <kyleonaut@gmail.com>
2022-09-22 17:45:25 +0200

    add thread safety

--- src/main/java/de/kyleonaut/risk/holder/CurrentGameHolder.java
+++ src/main/java/de/kyleonaut/risk/holder/CurrentGameHolder.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public class CurrentGameHolder {
     private Game instance;
 
-    public Game copy() {
+    public synchronized Game copy() {
         if (this.instance == null) {
             this.instance = new Game();
             this.instance.setState(State.IDLE);
@@ -19,7 +19,7 @@ public class CurrentGameHolder {
         return new Game(this.instance.getUuid(), this.instance.getParticipants(), this.instance.getState());
     }
 
-    public void update(Game game) {
+    public synchronized void update(Game game) {
         this.instance = game;
     }
 }
